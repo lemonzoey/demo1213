0223ec82c28ec10c74abb158e80d06bcd821ef56
zhouyi7 <zhouyi7@Kingsoft.com>
2023-03-24 16:19:55 +0800

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+
+const aa = '2222'
+const bb = `1111${aa}`
+console.log("🚀 ~ file: index.js:5 ~ bb", bb)
+
+
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+</template>
+
+<script>
+import lodash from 'lodash'
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  },
+  created(){
+    const base = {
+      aps1:{
+        host:'111',
+        re:'222'
+      }
+    }
+    const conf = {
+      aps1: {
+        ccc:'333',
+        host:'444'
+      }
+    }
+
+    const obj = {
+      aps1: {
+        host:'555',
+        re:'555'
+      }
+    }
+    const aa = lodash.defaultsDeep(conf,base,obj)
+    // const bb = Object.assign(conf,base,obj)
+    console.log("🚀 ~ file: HelloWorld.vue:32 ~ created ~ aa", aa)
+
+  }
+}
+</script>
